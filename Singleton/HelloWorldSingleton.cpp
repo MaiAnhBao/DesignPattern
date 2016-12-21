efbec09e20e7ec47d479bbaabc9376f6ed351801
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-12-21 17:50:08 +0700

    Singleton Rules

---
+++ Singleton/HelloWorldSingleton.cpp
@@ -0,0 +1,44 @@
+/*
+ * HelloWorldSingleton.cpp
+ *
+ *  Created on: 21 déc. 2016
+ *      Author: hnnguyen
+ */
+
+#include <iostream>
+
+class HelloWorld {
+private:
+	std::string mGreetingSentence;
+	static HelloWorld *mInstance;
+	HelloWorld(std::string strGreetingSentence = "") {
+		mGreetingSentence = strGreetingSentence;
+	}
+
+public:
+	static HelloWorld *getInstance() {
+		if (!mInstance) {
+			mInstance = new HelloWorld;
+		}
+		return mInstance;
+	}
+
+	void setGreetingSentence(std::string str) {
+		mGreetingSentence = str;
+	}
+
+	void sayHelloWorld() {
+		std::cout << mGreetingSentence;
+	}
+};
+
+
+HelloWorld *HelloWorld::mInstance = 0;
+
+int main(int argc, char **argv) {
+	HelloWorld::getInstance()->setGreetingSentence("Hello World\n");
+	HelloWorld::getInstance()->sayHelloWorld();
+	HelloWorld::getInstance()->setGreetingSentence("Bonjour Tout le monde\n");
+	HelloWorld::getInstance()->sayHelloWorld();
+	return 0;
+}

bd5cdd1c24652cc0513ecf2f15205cd96032a003
Mai Anh Bao <chuyendungdangki@yahoo.com>
2016-12-26 11:53:27 +0700

    State

---
+++ 03_BehavioralPatterns/State/State.cpp
@@ -0,0 +1,125 @@
+/*
+ * State.cpp
+ *
+ *  Created on: 26 déc. 2016
+ *      Author: hnnguyen
+ */
+
+#include <iostream>
+
+class HelloWorldMachine {
+	class State *current;
+public:
+	HelloWorldMachine();
+	void setCurrentState(State *s) {
+		current = s;
+	}
+
+	void eng();
+	void fr();
+	void es();
+};
+
+class State {
+public:
+	virtual void eng(HelloWorldMachine *m) {
+		std::cout << "Hello World\n";
+	}
+
+	virtual void fr(HelloWorldMachine *m) {
+		std::cout << "Bonjour tout le monde\n";
+	}
+
+
+	virtual void es(HelloWorldMachine *m) {
+		std::cout << "Hola el mundo \n";
+	}
+
+	virtual ~State() = default;
+};
+
+void HelloWorldMachine::eng() {
+	current->eng(this);
+}
+
+void HelloWorldMachine::fr() {
+	current->fr(this);
+}
+
+void HelloWorldMachine::es() {
+	current->es(this);
+}
+
+class HelloWorldEng: public State {
+public:
+	HelloWorldEng() {
+		std::cout << "Constructor Hello World Eng\n";
+	}
+
+	~HelloWorldEng() {
+		std::cout << "Destructor Hello World Eng\n";
+	}
+
+	void change(HelloWorldMachine *m);
+};
+
+class HelloWorldFr: public State {
+public:
+	HelloWorldFr() {
+		std::cout << "Constructor Hello World Fr\n";
+	}
+
+	~HelloWorldFr() {
+		std::cout << "Destructor Hello World Fr\n";
+	}
+
+	void change(HelloWorldMachine *m);
+};
+
+class HelloWorldEs: public State {
+public:
+	HelloWorldEs() {
+		std::cout << "Constructor Hello World Es\n";
+	}
+
+	~HelloWorldEs() {
+		std::cout << "Destructor Hello World Es\n";
+	}
+
+	void change(HelloWorldMachine *m) {
+		std::cout << "Change from FR to ENG";
+		m->setCurrentState(new HelloWorldEng());
+		delete this;
+	}
+};
+
+void HelloWorldFr::change(HelloWorldMachine *m) {
+	std::cout << "Change from FR to ES";
+	m->setCurrentState(new HelloWorldEs());
+	delete this;
+}
+
+void HelloWorldEng::change(HelloWorldMachine *m) {
+	std::cout << "Change from ENG to FR";
+	m->setCurrentState(new HelloWorldFr());
+	delete this;
+}
+
+HelloWorldMachine::HelloWorldMachine() {
+	current = new HelloWorldEng();
+	std::cout << std::endl;
+}
+
+int main(int argc, char **argv) {
+	void(HelloWorldMachine:: *ptrs[])() = {
+		&HelloWorldMachine::eng, &HelloWorldMachine::fr, &HelloWorldMachine::es
+	};
+	HelloWorldMachine fsm;
+	int num;
+	while (1) {
+		std::cout << "Enter 0/1/2: ";
+		std::cin >> num;
+		(fsm.*ptrs[num])();
+	}
+	return 0;
+}
